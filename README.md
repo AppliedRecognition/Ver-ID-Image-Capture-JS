a2cc13d2900c1869b64472b8905b674d11b8ff9d
Jakub Dolejs <jakubdolejs@gmail.com>
2021-03-18 19:06:01 +0000

    Added option to scale the image after capture.

--- README.md
+++ README.md
@@ -23,6 +23,14 @@ var button = document.createElement("a")
 button.href = "javascript:void(0)"
 // Attach a click listener
 button.onclick = function() {
+    var options = {
+        "useFrontCamera": true,
+        "size": {
+            "width": 600,
+            "height": 400
+        },
+        "scaling": "fill"
+    }
     // Capture the image
     captureImage().then(function(imageDataURL) {
         // Display the captured image
@@ -46,6 +54,45 @@ generateQRCode(location.href).then(function(qrCode) {
     img.src = qrCode
     document.body.appendChild(img)
 }).catch()
+```
+
+## Capture options
+All settings are optional.
+
+### useFrontCamera (boolean)
+Set to `true` to use the camera on the front of the device (facing the user). Set to `false` to use the camera on the back of the device (facing the environment).
+
+### size (object)
+Specify the size to which the captured image should be resized. If no size is specified the image will be left as it was captured by the camera.
+
+### size.width (number)
+Scale the image to match the given width.
+
+### size.height (number)
+Scale the image to match the given height.
+
+### scaling (string)
+Only applicable if both `size.width` and `size.height` are specified. Otherwise it's ignored.
+
+Possible values (red rectangle denotes specified width and height):
+
+- **fit** (default) – scale the image to fit into the given size preserving the image's aspect ratio<br /> ![Original](./doc_images/Diana_Ross_1976.jpg) ![Fit](./doc_images/fit.jpg)
+- **fill** – scale the image to fill the given size preserving the image's aspect ratio<br /> ![Original](./doc_images/Diana_Ross_1976.jpg) ![Fill](./doc_images/fill.jpg)
+- **stretch** - scale the image to stretch into the given size<br /> ![Original](./doc_images/Diana_Ross_1976.jpg) ![Stretch](./doc_images/stretch.jpg)
+
+*Image [Motown Records](https://commons.wikimedia.org/wiki/File:Diana_Ross_1976.jpg), Public domain, via Wikimedia Commons*
+
+## Example capture options
+
+```javascript
+{
+    "useFrontCamera": true, // capture a selfie
+    "size": { // scale the image to 300 x 300 pixels
+        "width": 300,
+        "height": 300
+    },
+    "scaling": "fill" // crop the image to fill the 300 x 300 pixel rectangle
+}
 ```
 
 ## Demo
--- src/imageCapture.js
+++ src/imageCapture.js
@@ -1,4 +1,4 @@
-export default function(useFrontCamera) {
+export default function(options) {
     return new Promise(function(resolve, reject) {
         function cleanup() {
             if (fileInput && fileInput.parentNode) {
@@ -9,7 +9,14 @@ export default function(useFrontCamera) {
         var fileInput = document.createElement("input")
         fileInput.type = "file"
         fileInput.setAttribute("accept", "image/*")
-        fileInput.setAttribute("capture", useFrontCamera ? "user" : "environment")
+        if (typeof options === "boolean") {
+            var useFrontCamera = options
+            fileInput.setAttribute("capture", useFrontCamera ? "user" : "environment")
+        } else if (typeof options === "object" && typeof options.useFrontCamera === "boolean") {
+            fileInput.setAttribute("capture", options.useFrontCamera ? "user" : "environment")
+        } else {
+            fileInput.setAttribute("capture", "environment")
+        }
         fileInput.style.opacity = 0;
         fileInput.style.position = "absolute";
         fileInput.style.bottom = "-30px";
@@ -20,9 +27,68 @@ export default function(useFrontCamera) {
             fileReader.onload = function(e) {
                 var dataURL = e.target.result
                 cleanup()
-                setTimeout(function() {
-                    resolve(dataURL)
-                })
+                if (typeof options === "object" && options.size && (options.size.width || options.size.height)) {
+                    var canvas = document.createElement("canvas")
+                    var context = canvas.getContext("2d", {"alpha": false, "desynchronized": true})
+                    var image = new Image()
+                    image.onerror = function() {
+                        reject("Failed to load image")
+                    }
+                    image.onload = function() {
+                        var x = 0, y = 0
+                        var scaleX = 1, scaleY = 1
+                        var width, height
+                        if (options.size.width && options.size.height) {
+                            if (options.scaling == "stretch") {
+                                scaleX = options.size.width / image.width
+                                scaleY = options.size.height / image.height
+                            } else if (options.scaling == "fill") {
+                                if (image.width / image.height > options.size.width / options.size.height) {
+                                    scaleX = scaleY = options.size.height / image.height
+                                } else {
+                                    scaleX = scaleY = options.size.width / image.width
+                                }
+                            } else {
+                                if (image.width / image.height > options.size.width / options.size.height) {
+                                    scaleX = scaleY = options.size.width / image.width
+                                } else {
+                                    scaleX = scaleY = options.size.height / image.height
+                                }
+                            }
+                        } else if (options.size.width) {
+                            scaleX = scaleY = options.size.width / image.width
+                        } else if (options.size.height) {
+                            scaleX = scaleY = options.size.height / image.height
+                        }
+                        if (scaleX > 1) {
+                            scaleY = 1 / scaleX * scaleY
+                            scaleX = 1
+                        }
+                        if (scaleY > 1) {
+                            scaleX = 1 / scaleY * scaleX
+                            scaleY = 1
+                        }
+                        width = image.width * scaleX
+                        height = image.height * scaleY
+                        if (options.scaling == "fill" && options.size.width && options.size.height) {
+                            x = options.size.width / 2 - width / 2
+                            y = options.size.height / 2 - height / 2
+                            canvas.width = width + x * 2
+                            canvas.height = height + y * 2
+                        } else {
+                            canvas.width = width;
+                            canvas.height = height;
+                        }
+                        context.drawImage(image, x, y, width, height)
+                        dataURL = canvas.toDataURL("image/jpeg")
+                        resolve(dataURL)
+                    }
+                    image.src = dataURL
+                } else {
+                    setTimeout(function() {
+                        resolve(dataURL)
+                    })
+                }
             }
             fileReader.onerror = function(e) {
                 cleanup()
